27208507e040f6a0ae191bb3e783683ea1e9b7bd
ashwini_sedin <ashwini.k@sedintechnologies.com>
2026-06-10 08:15:10 +0000

    indentation and Refactor student controller using before_action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,60 +1,68 @@
 class StudentsController < ApplicationController
-    def index
-        @students=Student.all
-        if params[:search].present?
-            @students=@students.where(
-                "name LIKE ?OR email LIKE?",
-                "%#{params[:search]}%",
-                 "%#{params[:search]}%"
+  before_action :set_student, only: [ :show, :edit, :update, :destroy ]
+  def index
+    @students = Student.all
 
-            )
-        end
-     if params[:course].present?
+    if params[:search].present?
+      @students = @students.where(
+        "name LIKE ? OR email LIKE ?",
+        "%#{params[:search]}%",
+        "%#{params[:search]}%"
+      )
+    end
+
+    if params[:course].present?
       @students = @students.where(course: params[:course])
-     end
     end
+  end
 
-    def show
-        @student=Student.find(params[:id])
-    end
-    def new
-        @student=Student.new
-    end
+  def show
+  end
 
+  def new
+    @student = Student.new
+  end
 
-    def create
-        @student=Student.new(student_params)
-        if @student.save
-            redirect_to students_path
-        else
-            render :new
-        end
-    end
-    def edit
-        @student=Student.find(params[:id])
-    end
-    def update
-        @student=Student.find(params[:id])
-        if @student.update(student_params)
-            redirect_to students_path
-        else 
-            render :edit
-        end
-    end 
-    def destroy
-        @student=Student.find(params[:id])
-        @student.destroy
-        redirect_to students_path
-    end
-    private def student_params
-        params.require(:student).permit(
-            :name,
-            :email,
-            :age,
-            :course,
-            :city,
-            :marks
+  def create
+    @student = Student.new(student_params)
 
-        )
+    if @student.save
+      redirect_to students_path
+    else
+      render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @student.update(student_params)
+      redirect_to students_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @student.destroy
+    redirect_to students_path
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(
+      :name,
+      :email,
+      :age,
+      :course,
+      :city,
+      :marks
+    )
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,13 +1,13 @@
 class Student < ApplicationRecord
  validates :name, presence: true
  validates :email, presence: true, uniqueness: true
- validates :age, numericality: {greater_than: 0 }
+ validates :age, numericality: { greater_than: 0 }
  validates :course, presence: true
  validates :city, presence: true
-  def result
-    return "pass" if marks.present?&& marks >= 35
-    return "fail" if marks.present?
-    
-    "N?A"
-  end
+
+ def result
+  return "pass" if marks.present? && marks >= 35
+  return "fail" if marks.present?
+  "N/A"
+ end
 end
