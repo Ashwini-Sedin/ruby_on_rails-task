20dbaf748a4001b683744d3391fd680788a308b5
ashwini_sedin <ashwini.k@sedintechnologies.com>
2026-07-27 10:21:18 +0000

    Configure SMTP for production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,13 +61,20 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV.fetch("RENDER_EXTERNAL_HOSTNAME", "ruby-on-rails-task-application.onrender.com"), protocol: "https" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
-  #   password: Rails.application.credentials.dig(:smtp, :password),
-  #   address: "smtp.example.com",
-  #   port: 587,
-  #   authentication: :plain
-  # }
+ config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: ENV["MAIL_USERNAME"],
+  password: ENV["MAIL_PASSWORD"],
+  authentication: "plain",
+  enable_starttls_auto: true
+}
+
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
